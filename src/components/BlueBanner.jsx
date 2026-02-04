83dff53dbe851991886d3b6a91a7fd219f65aed7
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-04 18:02:58 +0100

    Blue Banner

--- src/components/BlueBanner.jsx
+++ src/components/BlueBanner.jsx
@@ -1,3 +1,26 @@
+import digital from "../assets/img/buy-comics-digital-comics.png";
+import merchandise from "../assets/img/buy-comics-merchandise.png";
+import subscription from "../assets/img/buy-comics-subscriptions.png";
+import locator from "../assets/img/buy-comics-shop-locator.png";
+import visa from "../assets/img/buy-dc-power-visa.svg";
+
 export default function BlueBanner() {
-    return <h2>BLUE BANNER</h2>;
+    const bannerItems = [
+        { text: "DIGITAL COMICS", icon: digital },
+        { text: "DC MERCHANDISE", icon: merchandise },
+        { text: "SUBSCRIPTION", icon: subscription },
+        { text: "COMIC SHOP LOCATOR", icon: locator },
+        { text: "DC POWER VISA", icon: visa },
+    ];
+
+    return (
+        <section className="blue-banner">
+            {bannerItems.map((item, index) => (
+                <div key={index} className="banner-item">
+                    <img src={item.icon} alt={item.text} className="banner-icon" />
+                    <span>{item.text}</span>
+                </div>
+            ))}
+        </section>
+    );
 }
