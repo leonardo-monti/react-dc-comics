6b75b9682014be59987d0e9e4da32a364962b448
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-04 18:58:46 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,44 @@
+import { footerLinks } from "../data/footerLinks";
+
+import facebook from "../assets/img/footer-facebook.png";
+import twitter from "../assets/img/footer-twitter.png";
+import youtube from "../assets/img/footer-youtube.png";
+import pinterest from "../assets/img/footer-pinterest.png";
+import periscope from "../assets/img/footer-periscope.png";
+
+import dcLogoBg from "../assets/img/dc-logo-bg.png";
+import footerBg from "../assets/img/footer-bg.jpg";
+
 export default function Footer() {
-    return <h2>FOOTER</h2>;
+    return (
+        <footer className="footer">
+            <div
+                className="footer-top"
+                style={{ backgroundImage: `url(${dcLogoBg})` }}
+            >
+                {Object.entries(footerLinks).map(([title, links]) => (
+                    <div key={title} className="footer-column">
+                        <h4>{title}</h4>
+                        <ul>
+                            {links.map((link, index) => (
+                                <li key={index}>{link}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+
+            <div className="footer-bottom" style={{ backgroundColor: "#555" }}>
+                <button className="signup-button">SIGN-UP NOW!</button>
+                <div className="social-links">
+                    <img src={facebook} alt="Facebook" />
+                    <img src={twitter} alt="Twitter" />
+                    <img src={youtube} alt="YouTube" />
+                    <img src={pinterest} alt="Pinterest" />
+                    <img src={periscope} alt="Periscope" />
+                </div>
+            </div>
+        </footer>
+    );
 }
+
---
+++ src/data/footerLinks.js
@@ -0,0 +1,6 @@
+export const footerLinks = {
+    dcComics: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"],
+    dc: ["Terms Of Use", "Privacy Policy", "Ad Choices", "Jobs"],
+    sites: ["DC", "MAD Magazine", "DC Kids", "DC Universe"],
+    shop: ["Shop DC", "Shop DC Collectibles"],
+}
