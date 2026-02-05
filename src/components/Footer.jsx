3c38efa2cef4965c198b482fb22ac19d6f8503a0
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-05 16:51:26 +0100

    Footer Fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,30 +5,34 @@ import twitter from "../assets/img/footer-twitter.png";
 import youtube from "../assets/img/footer-youtube.png";
 import pinterest from "../assets/img/footer-pinterest.png";
 import periscope from "../assets/img/footer-periscope.png";
-
 import dcLogoBg from "../assets/img/dc-logo-bg.png";
 import footerBg from "../assets/img/footer-bg.jpg";
 
 export default function Footer() {
     return (
         <footer className="footer">
+
             <div
                 className="footer-top"
-                style={{ backgroundImage: `url(${dcLogoBg})` }}
+                style={{ backgroundImage: `url(${footerBg})` }}
             >
-                {Object.entries(footerLinks).map(([title, links]) => (
-                    <div key={title} className="footer-column">
-                        <h4>{title}</h4>
+                <img src={dcLogoBg} alt="DC Logo Big" className="dc-logo-big" />
+
+                {footerLinks.map((col, index) => (
+                    <div key={index} className="footer-column">
+                        <h4>{col.title}</h4>
                         <ul>
-                            {links.map((link, index) => (
-                                <li key={index}>{link}</li>
+                            {col.links.map((link, index) => (
+                                <li key={index}>
+                                    <a href="#" className="footer-link">{link}</a>
+                                </li>
                             ))}
                         </ul>
                     </div>
                 ))}
             </div>
 
-            <div className="footer-bottom" style={{ backgroundColor: "#555" }}>
+            <div className="footer-bottom">
                 <button className="signup-button">SIGN-UP NOW!</button>
                 <div className="social-links">
                     <img src={facebook} alt="Facebook" />
@@ -39,6 +43,5 @@ export default function Footer() {
                 </div>
             </div>
         </footer>
-    );
+    )
 }
-
--- src/data/footerLinks.js
+++ src/data/footerLinks.js
@@ -1,6 +1,18 @@
-export const footerLinks = {
-    dcComics: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"],
-    dc: ["Terms Of Use", "Privacy Policy", "Ad Choices", "Jobs"],
-    sites: ["DC", "MAD Magazine", "DC Kids", "DC Universe"],
-    shop: ["Shop DC", "Shop DC Collectibles"],
-}
+export const footerLinks = [
+    {
+        title: "DC COMICS",
+        links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos"]
+    },
+    {
+        title: "SHOP",
+        links: ["Shop DC", "Shop DC Collectibles"]
+    },
+    {
+        title: "DC",
+        links: ["Terms Of Use", "Privacy Policy", "Ad Choices", "Advertising", "Jobs"]
+    },
+    {
+        title: "SITES",
+        links: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]
+    }
+]
