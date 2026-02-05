1ed5508014bba488f4ee8f45c6b3cd9c86f562de
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-05 18:08:07 +0100

    add comics cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,21 @@
+import { comics } from "../data/comics.js";
+
 export default function Main() {
     return (
         <main className="main">
-            <h2>Content goes here</h2>
+
+            <div className="comics-container">
+                {comics.map((comic) => (
+                    <div key={comic.id} className="comic-card">
+                        <img src={comic.thumb} alt={comic.title} />
+                        <h5>{comic.title}</h5>
+                    </div>
+                ))}
+            </div>
+
+            <button className="load-more">LOAD MORE</button>
+
+
         </main>
     )
 }
